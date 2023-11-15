9ac2ec431d1b909a6eefb9e2aa4c8b7715c5a729
joakimjrr <140987073+joakimjrr@users.noreply.github.com>
2023-11-15 12:50:10 -0300

    Add files via upload

---
+++ src/components/MeuComponente.jsx
@@ -0,0 +1,6 @@
+export default function MeuComponente() {
+    return (
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda eaque corporis nemo cumque iste fugit molestiae non repellendus, aliquid optio quia voluptates dolorem, quis asperiores facere odio perferendis perspiciatis voluptatum.</p>
+    )
+  }
+
